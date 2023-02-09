2970659be8754e45e90def2e8faedf6238307614
M. Atif Siddiqui <56283783+atifsiddiqui10@users.noreply.github.com>
2023-02-09 10:09:00 -0600

    Update StatsApi.js

--- StatsApi.js
+++ StatsApi.js
@@ -10,7 +10,7 @@ class StatsAPI {
     outlet = "";
     secKey = ''
     constructor(){
-        this.oauthUrl = ``
+        this.oauthUrl = `https://oauth.performgroup.com/oauth/token`
     }
 
 
@@ -146,18 +146,69 @@ class StatsAPI {
         //console.log(comp_teams);
         return poss;
     }
-    // need to be updated 
-    async seasonalStats(tourID, token){
-        const response = await fetch(`https://api.performfeeds.com/soccerdata/seasonalstats/${this.outlet}?_rt=b&_fmt=json&fx=${matchID}&_pgSz=500`, {
+    //SeasonalStats
+    async seasonalStats(tourID, teamId, token){
+        const response = await fetch(`https://api.performfeeds.com/soccerdata/seasonstats/${this.outlet}?_rt=b&_fmt=json&tmcl=${tourID}&ctst=${teamId}&_pgSz=500`, {
             method: "GET",
             headers: {
                 'Authorization': `Bearer ${token}`,
             }
         })
-        const poss = await response.json();
+        const stats = await response.json();
         //console.log(comp_teams);
-        return poss;
+        return stats;
     }
+    //Transfers
+    async tranfers(tourID, token){
+        const response = await fetch(`https://api.performfeeds.com/soccerdata/transfers/${this.outlet}?_rt=b&_fmt=json&tmcl=${tourID}&_pgSz=500`, {
+            method: "GET",
+            headers: {
+                'Authorization': `Bearer ${token}`,
+            }
+        })
+        const tran = await response.json();
+        //console.log(comp_teams);
+        return tran;
+    }
+    // Rankings
+    async rankings(tourID, token){
+        const response = await fetch(`https://api.performfeeds.com/soccerdata/rankings/${this.outlet}?_rt=b&_fmt=json&tmcl=${tourID}&_pgSz=500`, {
+            method: "GET",
+            headers: {
+                'Authorization': `Bearer ${token}`,
+            }
+        })
+        const rank = await response.json();
+        //console.log(comp_teams);
+        return rank;
+    }
+
+    async topPerformers(tourID, token){
+        const response = await fetch(`https://api.performfeeds.com/soccerdata/topperformers/${this.outlet}?_rt=b&_fmt=json&tmcl=${tourID}&_pgSz=500`, {
+            method: "GET",
+            headers: {
+                'Authorization': `Bearer ${token}`,
+            }
+        })
+        const top = await response.json();
+        //console.log(comp_teams);
+        return top;
+    }
+
+    async commentary(matchID, token){
+        const response = await fetch(`https://api.performfeeds.com/soccerdata/commentary/${this.outlet}?_rt=b&_fmt=json&fx=${matchID}&_pgSz=500&type=fallback`, {
+            method: "GET",
+            headers: {
+                'Authorization': `Bearer ${token}`,
+            }
+        })
+        const comment = await response.json();
+        //console.log(comp_teams);
+        return comment;
+    }
+
+
+
 }
 
 /*
@@ -175,6 +226,14 @@ const mls2022TourCalID = '8xix0odg0gckp6uu1eg05mnmc'
 const MLS2022Teams = await stats.teamsByTC(mls2022TourCalID, accessToken);
 //console.log(MLS2022Teams.contestant.length)
 const mls22Teams = JSON.stringify(MLS2022Teams.contestant, null, "\t")
+
+let mlsTeamsIds = []
+MLS2022Teams.contestant.forEach(team => {
+    mlsTeamsIds.push(team.id)
+})
+
+console.log(mlsTeamsIds)
+
 
 //get mls 2022 squads
 const mls2022Squad = await stats.squads(mls2022TourCalID, accessToken);
@@ -248,54 +307,78 @@ async function mongoInsert(collecName, id, object){
         collection.insertOne({"_id":id,"optaMatchId":id,"matchPoss": object});
     }
 
+    else if(collecName == "mlsSeasonStats"){
+        const collection = db.collection(collecName);
+        collection.insertOne({"_id":id,"optaTeamId":id,"SeasonStats": object});
+    }
+
+    else if(collecName == "mlsTransfers"){
+        const collection = db.collection(collecName);
+        collection.insertOne({"_id":id,"optaTourCalId":id,"Transfers": object});
+    }
+
+    else if(collecName == "mlsRankings"){
+        const collection = db.collection(collecName);
+        collection.insertOne({"_id":id,"optaTourCalId":id,"Rankings": object});
+    }
+
+    else if(collecName == "topPerformers"){
+        const collection = db.collection(collecName);
+        collection.insertOne({"_id":id,"optaTourCalId":id,"TopPerformers": object});
+    }
+
+    else if(collecName == "mlsCommentary"){
+        const collection = db.collection(collecName);
+        collection.insertOne({"_id":id,"optaMatchId":id,"Commentary": object});
+    }
 }
 
 //MlS 2022 Teams
-// MLS2022Teams.contestant.forEach((team, index, array) => {
-//     const teamId = team.id
-//     const data = team
-//     mongoInsert("mlsTeams", teamId, data)
-// });
+MLS2022Teams.contestant.forEach((team, index, array) => {
+    const teamId = team.id
+    const data = team
+    mongoInsert("mlsTeams", teamId, data)
+});
 
 //MLS team squads 
 //console.log(mls2022Squad)
-// mls2022Squad.squad.forEach((squad, index, obj)=>{
-//     const teamId = squad.contestantId
-//     const teamSquad = squad
-//     console.log(teamId)
-//     mongoInsert('mls22Squads', teamId, teamSquad)
-// })
+mls2022Squad.squad.forEach((squad, index, obj)=>{
+    const teamId = squad.contestantId
+    const teamSquad = squad
+    console.log(teamId)
+    mongoInsert('mls22Squads', teamId, teamSquad)
+})
 
 //mls 2022 fix
-// console.log(mls2022Fixutes.match)
-// mls2022Fixutes.match.forEach((fixture, index, array)=>{
-//     const fixId = fixture.matchInfo.id
-//     const fixInfo = fixture.matchInfo
-//     mongoInsert('mls22Fixtures', fixId, fixInfo)
-// })
+console.log(mls2022Fixutes.match)
+mls2022Fixutes.match.forEach((fixture, index, array)=>{
+    const fixId = fixture.matchInfo.id
+    const fixInfo = fixture.matchInfo
+    mongoInsert('mls22Fixtures', fixId, fixInfo)
+})
 
 
 // Match Stats 
-// for (let x = 0; x < mls2022MatchIds.length; x ++){
-//     let matchId = mls2022MatchIds[x];
-//     const matchStats = await stats.matchStats(matchId, accessToken);
-//     console.log(matchStats.matchInfo.id)
-//     const mId = matchStats.matchInfo.id
-//     const mStat = matchStats
-//     // mongoInsert('mlsMatchStats', mId,mStat)
+for (let x = 0; x < mls2022MatchIds.length; x ++){
+    let matchId = mls2022MatchIds[x];
+    const matchStats = await stats.matchStats(matchId, accessToken);
+    console.log(matchStats.matchInfo.id)
+    const mId = matchStats.matchInfo.id
+    const mStat = matchStats
+    // mongoInsert('mlsMatchStats', mId,mStat)
 
-// }
+}
 
 // Match Events 
-// for (let x = 0; x < mls2022MatchIds.length; x ++){
-//     let matchId = mls2022MatchIds[x];
-//     const matchEvents = await stats.matchEvent(matchId, accessToken)
-//     console.log(matchEvents.matchInfo.id)
-//     const mId = matchEvents.matchInfo.id
-//     const mEvent = matchEvents
-//     mongoInsert('mlsMatchEvents', mId,mEvent)
+for (let x = 0; x < mls2022MatchIds.length; x ++){
+    let matchId = mls2022MatchIds[x];
+    const matchEvents = await stats.matchEvent(matchId, accessToken)
+    console.log(matchEvents.matchInfo.id)
+    const mId = matchEvents.matchInfo.id
+    const mEvent = matchEvents
+    mongoInsert('mlsMatchEvents', mId,mEvent)
 
-// }
+}
 
 // Match Pass Matrix and avg Positions 
 for (let x = 0; x < mls2022MatchIds.length; x ++){
@@ -309,26 +392,50 @@ for (let x = 0; x < mls2022MatchIds.length; x ++){
 }
 
 // // Match Possession 
-// for (let x = 0; x < mls2022MatchIds.length; x ++){
-//     let matchId = mls2022MatchIds[x];
-//     const poss = await stats.possession(matchId, accessToken)
-//     // console.log(passMatrix)
-//     const mId = poss.matchInfo.id
-//     const mPass = poss
-//     mongoInsert('mlsMatchPassMat', mId,mPass)
+for (let x = 0; x < mls2022MatchIds.length; x ++){
+    let matchId = mls2022MatchIds[x];
+    const poss = await stats.possession(matchId, accessToken)
+    //console.log(poss)
+    const mId = poss.matchInfo.id
+    const mPoss = poss
+    mongoInsert('mlsMatchPoss', mId,mPoss)
 
-// }
+}
 
+//Seasonal Stats 
+
+for (let x = 0; x < mlsTeamsIds.length; x++){
+    let teamId = mlsTeamsIds[x];
+    //console.log(teamId)
+    //console.log(mls2022TourCalID)
+    const seasStats = await stats.seasonalStats(mls2022TourCalID, teamId, accessToken)
+    const tId = teamId
+    const sStat = seasStats
+    mongoInsert('mlsSeasonStats', tId, sStat)
+    //console.log(seasStats)
+}
 
 
 //transfers 
-
-// Detailed Match Stats 
-
-//season stats 
+const mls22Transfers = await stats.tranfers(mls2022TourCalID, accessToken)
+mongoInsert('mlsTransfers',mls2022TourCalID,mls22Transfers)
 
 // Ranking
+const mls22Rankings = await stats.rankings(mls2022TourCalID, accessToken)
+mongoInsert('mlsRankings',mls2022TourCalID,mls22Rankings)
 
 //Top performers 
+const mls22TopPerformers = await stats.topPerformers(mls2022TourCalID, accessToken)
+mongoInsert('topPerformers',mls2022TourCalID,mls22TopPerformers)
 
 // auto commentating 
+
+for (let x = 0; x < mls2022MatchIds.length; x ++){
+    let matchId = mls2022MatchIds[x];
+    const com = await stats.commentary(matchId, accessToken)
+    //console.log(poss)
+    const mId = matchId
+    const mCom = com
+    mongoInsert('mlsCommentary', mId,mCom)
+
+}
